8afb4e25b39d2080fb7f789f6e7d64d2750d10fa
charanreddy18-vk <59085132+charanreddy18-vk@users.noreply.github.com>
2020-08-03 03:33:55 -0400

    lets c

--- properproject/src/properproject/involve.java
+++ properproject/src/properproject/involve.java
@@ -2,8 +2,10 @@ package properproject;
 
 public class involve {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
+		System.out.println("small change in the code lets see how does it works");
 
 	}
 
